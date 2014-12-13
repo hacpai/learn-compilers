2e8492dca5275dd47cf7956720ef841f990e57db
arcticlion <david.malan@aol.com>
2014-12-14 01:04:22 +0800

    Refine coding style standards

--- sum-stack-compiler.c
+++ sum-stack-compiler.c
@@ -2,68 +2,68 @@
 #include <stdlib.h>
 
 #define TODO()                                  \
-  do{                                                           \
-    printf ("\nAdd your code here: file \"%s\", line %d\n",     \
+    do{                                                           \
+        printf ("\nAdd your code here: file \"%s\", line %d\n",     \
             __FILE__, __LINE__);                                \
-  }while(0)
+    } while(0)
 
 ///////////////////////////////////////////////
 // Data structures for the Sum language.
 enum Exp_Kind_t {EXP_INT, EXP_SUM};
 struct Exp_t
 {
-  enum Exp_Kind_t kind;
+    enum Exp_Kind_t kind;
 };
 
 struct Exp_Int
 {
-  enum Exp_Kind_t kind;
-  int i;
+    enum Exp_Kind_t kind;
+    int i;
 };
 
 struct Exp_Sum
 {
-  enum Exp_Kind_t kind;
-  struct Exp_t *left;
-  struct Exp_t *right;
+    enum Exp_Kind_t kind;
+    struct Exp_t *left;
+    struct Exp_t *right;
 };
 
 // "constructors"
 struct Exp_t *Exp_Int_new (int i)
 {
-  struct Exp_Int *p = malloc (sizeof(*p));
-  p->kind = EXP_INT;
-  p->i = i;
-  return (struct Exp_t *)p;
+    struct Exp_Int *p = malloc (sizeof(*p));
+    p->kind = EXP_INT;
+    p->i = i;
+    return (struct Exp_t *)p;
 }
 
 struct Exp_t *Exp_Sum_new (struct Exp_t *left, struct Exp_t *right)
 {
-  struct Exp_Sum *p = malloc (sizeof(*p));
-  p->kind = EXP_SUM;
-  p->left = left;
-  p->right = right;
-  return (struct Exp_t *)p;
+    struct Exp_Sum *p = malloc (sizeof(*p));
+    p->kind = EXP_SUM;
+    p->left = left;
+    p->right = right;
+    return (struct Exp_t *)p;
 }
 
 // "printer"
-void Exp_print (struct Exp_t *exp)
+void Print_Exp (struct Exp_t *exp)
 {
-  switch (exp->kind){
-  case EXP_INT:{
-    struct Exp_Int *p = (struct Exp_Int *)exp;
-    printf ("%d", p->i);
-    break;
+    switch (exp->kind){
+        case EXP_INT:{
+            struct Exp_Int *p = (struct Exp_Int *)exp;
+            printf ("%d", p->i);
+            break;
   }
-  case EXP_SUM:{
-    struct Exp_Sum *p = (struct Exp_Sum *)exp;
-    Exp_print (p->left);
-    printf ("+");
-    Exp_print (p->right);
-    break;
+        case EXP_SUM:{
+            struct Exp_Sum *p = (struct Exp_Sum *)exp;
+            Print_Exp (p->left);
+            printf ("+");
+            Print_Exp (p->right);
+            break;
   }
-  default:
-    break;
+          default:
+            break;
   }
 }
 
@@ -72,96 +72,101 @@ void Exp_print (struct Exp_t *exp)
 enum Stack_Kind_t {STACK_ADD, STACK_PUSH};
 struct Stack_t
 {
-  enum Stack_Kind_t kind;
+    enum Stack_Kind_t kind;
 };
 
 struct Stack_Add
 {
-  enum Stack_Kind_t kind;
+    enum Stack_Kind_t kind;
 };
 
 struct Stack_Push
 {
-  enum Stack_Kind_t kind;
-  int i;
+    enum Stack_Kind_t kind;
+    int i;
 };
 
 // "constructors"
 struct Stack_t *Stack_Add_new ()
 {
-  struct Stack_Add *p = malloc (sizeof(*p));
-  p->kind = STACK_ADD;
-  return (struct Stack_t *)p;
+    struct Stack_Add *p = malloc (sizeof(*p));
+    p->kind = STACK_ADD;
+    return (struct Stack_t *)p;
 }
 
 struct Stack_t *Stack_Push_new (int i)
 {
-  struct Stack_Push *p = malloc (sizeof(*p));
-  p->kind = STACK_PUSH;
-  p->i = i;
-  return (struct Stack_t *)p;
+    struct Stack_Push *p = malloc (sizeof(*p));
+    p->kind = STACK_PUSH;
+    p->i = i;
+    return (struct Stack_t *)p;
 }
 
 /// instruction list
 struct List_t
 {
-  struct Stack_t *instr;
-  struct List_t *next;
+    struct Stack_t *instr;
+    struct List_t *next;
 };
 
 struct List_t *List_new (struct Stack_t *instr, struct List_t *next)
 {
-  struct List_t *p = malloc (sizeof (*p));
-  p->instr = instr;
-  p->next = next;
-  return p;
+    struct List_t *p = malloc (sizeof (*p));
+    p->instr = instr;
+    p->next = next;
+    return p;
 }
 
 // "printer"
-void List_reverse_print (struct List_t *list)
+void Reverse_print_list (struct List_t *list)
 {
+    if (list == NULL) return;
+    Reverse_print_list (list->next);
   //TODO();
-  switch (list->instr->kind){
-  case STACK_PUSH:{
-    struct Stack_Push *p = (struct Stack_Push *)list;
-    printf ("\npush %d", p->i);
-    break;
-  }
-  case STACK_ADD:{
-    struct Stack_Add *p = (struct Stack_Add *)list;
-    printf ("\nadd");
-    break;
-  }
-  default:
-    break;
-  }
+    switch (list->instr->kind) {
+        case STACK_PUSH: {
+            struct Stack_Push *p = (struct Stack_Push *)(list->instr);
+            printf ("\npush %d", p->i);
+            break;
+     }
+        case STACK_ADD: {
+            struct Stack_Add *p = (struct Stack_Add *)list;
+            printf ("\nadd");
+            list=list->next;
+            break;
+     }
+        default:
+            break;
+      }
 }
 
 //////////////////////////////////////////////////
 // a compiler from Sum to Stack
 struct List_t *all = 0;
 
-void emit (struct Stack_t *instr)
+void Emit (struct Stack_t *instr)
 {
-  all = List_new (instr, all);
+    all = List_new (instr, all);
 }
 
-void compile (struct Exp_t *exp)
+void Compile (struct Exp_t *exp)
 {
-  switch (exp->kind){
-  case EXP_INT:{
-    struct Exp_Int *p = (struct Exp_Int *)exp;
-    emit (Stack_Push_new (p->i));
-    break;
+    switch (exp->kind){
+        case EXP_INT:{
+            struct Exp_Int *p = (struct Exp_Int *)exp;
+            Emit (Stack_Push_new (p->i));
+            break;
   }
-  case EXP_SUM:{
     //TODO();
-    struct Exp_Sum *p = (struct Exp_Sum *)exp;
-    emit (Stack_Add_new ());
-    break;
+        case EXP_SUM:{
+            struct Exp_Sum *p = (struct Exp_Sum *)exp;
+            Compile (p->left);
+            Emit (Stack_Add_new ());
+            Compile (p->right);
+            break;
   }
-  default:
-    break;
+        default:
+            break;
   }
 }
 
@@ -169,26 +174,26 @@ void compile (struct Exp_t *exp)
 // program entry
 int main()
 {
-  printf("Compile starting\n");
-  // build an expression tree:
-  //            +
-  //           / \
-  //          +   4
-  //         / \
-  //        2   3
-  struct Exp_t *exp = Exp_Sum_new (Exp_Sum_new(Exp_Int_new (2)
+    printf("Compile starting\n");
+    // build an expression tree:
+    //            +
+    //           / \
+    //          +   4
+    //         / \
+    //        2   3
+    struct Exp_t *exp = Exp_Sum_new (Exp_Sum_new(Exp_Int_new (2)
                                                , Exp_Int_new (3))
                                    , Exp_Int_new (4));
-  // print out this tree:
-  printf ("the expression is:\n");
-  Exp_print (exp);
-  // compile this tree to Stack machine instructions
-  compile (exp);
+    // print out this tree:
+    printf ("the expression is:\n");
+    Print_Exp (exp);
+    // compile this tree to Stack machine instructions
+    Compile (exp);
 
-  // print out the generated Stack instructons:
-  List_reverse_print (all);
+    // print out the generated Stack instructons:
+    Reverse_print_list (all);
   
-  printf("\nCompile finished\n");
-  return 0;
+    printf("\nCompile finished\n");
+    return 0;
 }
 
